810d1bf8f6202a51728a58a9fd1d01c21656f09b
OliverSmithDev <72077531+OliverSmithDev@users.noreply.github.com>
2022-05-26 23:35:36 +0100

    Player health and enemy damage

---
+++ EndlessWaveDefence/Assets/Scripts/Enemy/EnemyDamage.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+
+    public PlayerHealth playerhealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+           playerhealth.Health -= 1;
+            print("HitPlayer");
+        }
+    }
+
+    
+
+
+}
