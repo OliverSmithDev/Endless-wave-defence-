1e916532e682fa6f8b16d525fad61dd785a781b9
OliverSmithDev <72077531+OliverSmithDev@users.noreply.github.com>
2022-05-27 21:23:57 +0100

    Enemy Health

--- EndlessWaveDefence/Assets/Scripts/GameManager/GameManager.cs
+++ EndlessWaveDefence/Assets/Scripts/GameManager/GameManager.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class GameManager : MonoBehaviour
 {
     public float HealthEnemy;
     public float HealthMulti = 1.5f;
     public float Timer;
-    public GameManager gamemanager;
+    public float MaxTimer;
 
     // Start is called before the first frame update
     void Start()
     {
-        HealthEnemy = gamemanager.HealthEnemy;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        Timer += Time.deltaTime;
+
+        if (Timer >= MaxTimer)
+        {
+            HealthEnemy = HealthEnemy * HealthMulti;
+            Timer = 0;
+        }
     }
 }
